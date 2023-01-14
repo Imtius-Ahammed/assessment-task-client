39831c59c395d4b6e9464d8ec5d094fd85774089
Imtius-Ahammed <ahammedimtius098@gmail.com>
2023-01-14 09:32:29 +0600

    minor fixes

--- src/Pages/Home/AllImages/AllImages.js
+++ src/Pages/Home/AllImages/AllImages.js
@@ -1,63 +1,65 @@
-import React, { useEffect, useState } from 'react';
-import AllImagesCard from './AllImagesCard';
-import "./AllImages.css"
-
-
+import React, { useEffect, useState } from "react";
+import AllImagesCard from "./AllImagesCard";
+import "./AllImages.css";
 
 const AllImages = () => {
   const [allData, setAllData] = useState([]);
 
-const [count,setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-const [page,setPage] = useState(0);
-const [size,setSize] = useState(10);
+  const [page, setPage] = useState(0);
+  const [size, setSize] = useState(10);
 
-useEffect(()=>{
- const url =  `http://localhost:5000/imgPagination?page=${page}&size=${size}`;
- fetch(url)
- .then(res=> res.json())
- .then(data=>{
-   setCount(data.count);
-   setAllData(data.imgPagination);
- })
+  useEffect(() => {
+    const url = `https://assessment-server.vercel.app/imgPagination?page=${page}&size=${size}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setCount(data.count);
+        setAllData(data.imgPagination);
+      });
+  }, [page, size]);
 
-},[page,size])
+  const pages = Math.ceil(count / size);
 
-const pages = Math.ceil(count/size);
-
-
-
-  
-  useEffect(()=>{
-    fetch('http://localhost:5000/allImage')
-    .then(res=>res.json())
-    .then(data=>setAllData(data))
-  },[])
-
+  useEffect(() => {
+    fetch("https://assessment-server.vercel.app/allImage")
+      .then((res) => res.json())
+      .then((data) => setAllData(data));
+  }, []);
 
   return (
-    <div >
-      <h1 className='text-center text-5xl font-medium my-10'>All Images Are Here</h1>
-     <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 py-5 container mx-auto'>
-     {
-        allData.map(images=><AllImagesCard  key={images._id} images={images}></AllImagesCard>)
-      }
-     </div>
+    <div>
+      <h1 className="text-center text-5xl font-medium my-10">
+        All Images Are Here
+      </h1>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 py-5 container mx-auto">
+        {allData.map((images) => (
+          <AllImagesCard key={images._id} images={images}></AllImagesCard>
+        ))}
+      </div>
       <div className="pagination">
-        <p>Currently Selected Page: {page} and size: {size}</p>
-        {
-          [...Array(pages).keys()].map(number=><button key={number} className={page === number && 'selected'} onClick={()=>setPage(number)}>{number+1}</button>)
-
-        }
-        <select onChange={event=>setSize(event.target.value)} >
+        <p>
+          Currently Selected Page: {page} and size: {size}
+        </p>
+        {[...Array(pages).keys()].map((number) => (
+          <button
+            key={number}
+            className={page === number && "selected"}
+            onClick={() => setPage(number)}
+          >
+            {number + 1}
+          </button>
+        ))}
+        <select onChange={(event) => setSize(event.target.value)}>
           <option value="5">5</option>
-          <option value="10" selected>10</option>
+          <option value="10" selected>
+            10
+          </option>
           <option value="15">15</option>
           <option value="20">20</option>
         </select>
-
       </div>
-      
     </div>
   );
 };
--- src/Pages/Home/ImageTable/ImageTable.js
+++ src/Pages/Home/ImageTable/ImageTable.js
@@ -1,40 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import TableDetails from './TableDetails';
+import React, { useEffect, useState } from "react";
+import TableDetails from "./TableDetails";
 
 const ImageTable = () => {
   const [allData, setAllData] = useState([]);
-  useEffect(()=>{
-    fetch('http://localhost:5000/allImage')
-    .then(res=>res.json())
-    .then(data=>setAllData(data))
-  },[])
+  useEffect(() => {
+    fetch("https://assessment-server.vercel.app/allImage")
+      .then((res) => res.json())
+      .then((data) => setAllData(data));
+  }, []);
   return (
     <div>
       <div className="overflow-x-auto w-full border rounded-lg shadow-md">
-  <table className="table w-full">
-  
-    <thead>
-      <tr>
-       
-        <th>Images</th>
-        <th>Name</th>
-       
-       
-      </tr>
-    </thead>
-    <tbody>
-     
-     {
-      allData.slice(0,5).map(data=><TableDetails key={data._id} data={data} ></TableDetails>)
-     }
-     
-    
-    </tbody>
-   
-
-    
-  </table>
-</div>
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>Images</th>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allData.slice(0, 5).map((data) => (
+              <TableDetails key={data._id} data={data}></TableDetails>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
